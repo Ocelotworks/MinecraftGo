36c4a8c087c61d90e7c52089388a2f3924171667
Byron Davies <byron.davies@lot.to>
2020-02-26 17:44:53 +0000

    Fixed writing negative var ints and writing var ints > 40 bits long

--- dataTypes/VarInt_test.go
+++ dataTypes/VarInt_test.go
@@ -1,42 +1,44 @@
 package dataTypes
 
 import (
-	_ "github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/assert"
 )
 
 func TestFixedValues(t *testing.T) {
-	//test, _ := ReadVarInt([]byte{0x00})
-	//assert.Equal(t, test, 0)
-	//
-	//test, _ = ReadVarInt([]byte{0x01})
-	//assert.Equal(t, test, 1)
-	//
-	//test, _ = ReadVarInt([]byte{0x02})
-	//assert.Equal(t, test, 2)
-	//
-	//test, _ = ReadVarInt([]byte{0x7f})
-	//assert.Equal(t, test, 127)
-	//
-	//test, _ = ReadVarInt([]byte{0x80, 0x01})
-	//assert.Equal(t, test, 128)
-	//
-	//test, _ = ReadVarInt([]byte{0xff, 0x01})
-	//assert.Equal(t, test, 255)
-	//
-	//test, _ = ReadVarInt([]byte{0xff, 0xff, 0xff, 0xff, 0x07})
-	//assert.Equal(t, test, 2147483647)
-	//
-	//test, _ = ReadVarInt([]byte{0xff, 0xff, 0xff, 0xff, 0x0f})
-	//assert.Equal(t, test, -1)
-	//
-	//test, _ = ReadVarInt([]byte{0x80, 0x80, 0x80, 0x80, 0x08})
-	//assert.Equal(t, test, -2147483648)
+	test, _ := ReadVarInt([]byte{0x00})
+	assert.Equal(t, test, 0)
+
+	test, _ = ReadVarInt([]byte{0x01})
+	assert.Equal(t, test, 1)
+
+	test, _ = ReadVarInt([]byte{0x02})
+	assert.Equal(t, test, 2)
+
+	test, _ = ReadVarInt([]byte{0x7f})
+	assert.Equal(t, test, 127)
+
+	test, _ = ReadVarInt([]byte{0x80, 0x01})
+	assert.Equal(t, test, 128)
+
+	test, _ = ReadVarInt([]byte{0xff, 0x01})
+	assert.Equal(t, test, 255)
+
+	test, _ = ReadVarInt([]byte{0xff, 0xff, 0xff, 0xff, 0x07})
+	assert.Equal(t, test, 2147483647)
+
+	test, _ = ReadVarInt([]byte{0xff, 0xff, 0xff, 0xff, 0x0f})
+	assert.Equal(t, test, -1)
+
+	test, _ = ReadVarInt([]byte{0x80, 0x80, 0x80, 0x80, 0x08})
+	assert.Equal(t, test, -2147483648)
 }
 
 func TestReadWrite(t *testing.T) {
-	for i := 0; i <= 2048; i++ {
-		//test, _ := ReadVarInt(WriteVarInt(i))
-		//	assert.Equal(t, i, test)
+	for i := -2048; i <= 2048; i++ {
+		test, _ := ReadVarInt(WriteVarInt(i))
+		assert.Equal(t, i, test)
 	}
 }
--- packet/PlayerPosition.go
+++ packet/PlayerPosition.go
@@ -10,9 +10,3 @@ type PlayerPosition struct {
 func (pp *PlayerPosition) GetPacketId() int {
 	return 0x36
 }
-
-/**
-func (pp *PlayerPosition) Handle(packet []byte, connection *Connection) {
-	connection.Minecraft.UpdatePlayerPosition(connection, pp.X, pp.FeetY, pp.Z, 0, 0)
-}
-*/
